3ff7dae93401aae42f10a3abb3a4ce52c8fd81fe
Leonardo José Consoni <consoni_2519@hotmail.com>
2017-06-19 13:37:33 -0300

    Update loader_macros.h

--- loader_macros.h
+++ loader_macros.h
@@ -2,20 +2,20 @@
 //                                                                            //
 //  Copyright (c) 2016-2017 Leonardo Consoni <consoni_2519@hotmail.com>       //
 //                                                                            //
-//  This file is part of Platform Utils.                                      //
+//  This file is part of Plugin Loader.                                       //
 //                                                                            //
-//  Platform Utils is free software: you can redistribute it and/or modify    //
+//  Plugin Loader is free software: you can redistribute it and/or modify     //
 //  it under the terms of the GNU Lesser General Public License as published  //
 //  by the Free Software Foundation, either version 3 of the License, or      //
 //  (at your option) any later version.                                       //
 //                                                                            //
-//  Platform Utils is distributed in the hope that it will be useful,         //
+//  Plugin Loader is distributed in the hope that it will be useful,          //
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of            //
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              //
 //  GNU Lesser General Public License for more details.                       //
 //                                                                            //
 //  You should have received a copy of the GNU Lesser General Public License  //
-//  along with Platform Utils. If not, see <http://www.gnu.org/licenses/>.    //
+//  along with Plugin Loader. If not, see <http://www.gnu.org/licenses/>.     //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
